0d99ba2db1ed62bba31b950212cc30ccccca7fba
agowa338 <agowa338@users.noreply.github.com>
2016-02-15 18:31:11 +0100

    Update ForcePaswordChange.ps1

--- ForcePaswordChange.ps1
+++ ForcePaswordChange.ps1
@@ -5,6 +5,32 @@
 ##                                                     ^^^^^
 ## ======================== END Settings ==================================
 
+## ========================== LICENSE =====================================
+
+## The MIT License (MIT)
+## 
+## Copyright (c) 2016 
+## 
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+## 
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+## 
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+
+## ======================== END LICENSE ===================================
+
 ## Change password on next Login => pwdLastSet -eq 0
 ## Where-Object pwdLastSet -NE 0   ==>>> Where User does not need to change password at next login
 ## PasswordLastSet => Human readable pwdLastSet
